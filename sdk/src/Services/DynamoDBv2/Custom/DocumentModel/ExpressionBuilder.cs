48a2debaab8af99d77c678c145459a51d17d70de
irina-herciu <irina.tanasa@endava.com>
2025-03-24 15:47:22 +0200

    ExpressionBuilder implementation

---
+++ sdk/src/Services/DynamoDBv2/Custom/DocumentModel/ExpressionBuilder.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Amazon.DynamoDBv2.DocumentModel
+{
+    /// <summary>
+    /// Builder that constructs a <see cref="Expression"/>
+    /// </summary>
+    public class ExpressionBuilder : IExpressionBuilder
+    {
+        private string _expression;
+        private Dictionary<string, DynamoDBEntry> _expressionValues;
+        private Dictionary<string, string> _expressionNames;
+
+        /// <summary>
+        /// Constructs a new ExpressionBuilder
+        /// </summary>
+        public ExpressionBuilder() { }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder AddExpression(string expression)
+        {
+            _expression = expression;
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder AddExpressionValues(Dictionary<string, DynamoDBEntry> expressionValues)
+        {
+            _expressionValues = expressionValues == null ? null : new Dictionary<string, DynamoDBEntry>(expressionValues);
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder MergeExpressionValues(Dictionary<string, DynamoDBEntry> expressionValues)
+        {
+            if (_expressionValues == null)
+                return AddExpressionValues(expressionValues);
+
+            if (expressionValues == null)
+                return this;
+
+            foreach (var kvp in expressionValues)
+            {
+                if (_expressionValues.TryGetValue(kvp.Key, out var oldValue))
+                {
+                    if (!EqualityComparer<DynamoDBEntry>.Default.Equals(oldValue, kvp.Value))
+                    {
+                        throw new ArgumentException($"Attempt to coalesce expressions with conflicting expression values. Expression value key = '{kvp.Key}'");
+                    }
+                }
+
+                _expressionValues[kvp.Key] = kvp.Value;
+            }
+
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder PutExpressionValue(string key, DynamoDBEntry value)
+        {
+            _expressionValues ??= new Dictionary<string, DynamoDBEntry>();
+            _expressionValues[key] = value;
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public ExpressionBuilder AddExpressionNames(Dictionary<string, string> expressionNames)
+        {
+            _expressionNames = expressionNames == null ? null : new Dictionary<string, string>(expressionNames);
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder MergeExpressionNames(Dictionary<string, string> expressionNames)
+        {
+            if (_expressionNames == null)
+                return AddExpressionNames(expressionNames);
+
+            if (expressionNames == null)
+                return this;
+
+            foreach (var kvp in expressionNames)
+            {
+                if (_expressionNames.TryGetValue(kvp.Key, out var oldValue))
+                {
+                    if (!string.Equals(oldValue, kvp.Value))
+                    {
+                        throw new ArgumentException($"Attempt to coalesce expressions with conflicting expression names. Expression name key = '{kvp.Key}'");
+                    }
+                }
+
+                _expressionNames[kvp.Key] = kvp.Value;
+            }
+
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public ExpressionBuilder PutExpressionName(string key, string value)
+        {
+            _expressionNames ??= new Dictionary<string, string>();
+            _expressionNames[key] = value;
+            return this;
+        }
+
+
+        /// <inheritdoc/>
+        public Expression Build()
+        {
+            return new Expression()
+            {
+                ExpressionStatement = _expression,
+                ExpressionAttributeNames = _expressionNames,
+                ExpressionAttributeValues = _expressionValues
+            };
+        }
+
+    }
+}
---
+++ sdk/src/Services/DynamoDBv2/Custom/DocumentModel/IExpressionBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Amazon.DynamoDBv2.DocumentModel
+{
+    /// <summary>
+    /// Interface for a builder that constructs a <see cref="Expression"/>
+    /// </summary>
+    public interface IExpressionBuilder
+    {
+        /// <summary>
+        /// The expression string
+        /// </summary>
+        ExpressionBuilder AddExpression(string expression);
+
+        /// <summary>
+        /// The optional 'expression values' string dictionary
+        /// </summary>
+        ExpressionBuilder AddExpressionValues(Dictionary<string, DynamoDBEntry> expressionValues);
+
+        /// <summary>
+        /// Merge the given ExpressionValues into the builder's existing ExpressionValues
+        /// </summary>
+        ExpressionBuilder MergeExpressionValues(Dictionary<string, DynamoDBEntry> expressionValues);
+
+        /// <summary>
+        /// Adds a single element to the optional 'expression values' string dictionary
+        /// </summary>
+        ExpressionBuilder PutExpressionValue(string key, DynamoDBEntry value);
+
+        /// <summary>
+        /// The optional 'expression names' string dictionary
+        /// </summary>
+        ExpressionBuilder AddExpressionNames(Dictionary<string, string> expressionNames);
+
+        /// <summary>
+        /// Merge the given ExpressionNames into the builder's existing ExpressionNames
+        /// </summary>
+        ExpressionBuilder MergeExpressionNames(Dictionary<string, string> expressionNames);
+
+        /// <summary>
+        /// Adds a single element to the optional 'expression names' string dictionary
+        /// </summary>
+        ExpressionBuilder PutExpressionName(string key, string value);
+
+        /// <summary>
+        /// Call at the end to build the new <see cref="Expression"/>
+        /// </summary>
+        Expression Build();
+    }
+}
