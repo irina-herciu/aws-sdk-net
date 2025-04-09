d2c47d16d902f9d1579b4f8c01b30e8624f27667
irina-herciu <irina.tanasa@endava.com>
2025-04-09 15:57:59 +0300

    remove unit tests

--- sdk/test/Services/DynamoDBv2/UnitTests/Custom/ExpressionBuilderTests.cs
+++ sdk/test/Services/DynamoDBv2/UnitTests/Custom/ExpressionBuilderTests.cs
@@ -8,66 +8,6 @@ namespace AWSSDK_DotNet.UnitTests
     public class ExpressionBuilderTests
     {
         [TestMethod]
-        public void TestMethod3()
-        {
-            var condition =
-                ConditionalExpression.New().WithName("Price").EqualTo(50)
-                .Build();
-
-            Assert.AreEqual("#price = :price", condition.ExpressionStatement);
-            Assert.IsTrue(condition.ExpressionAttributeNames.ContainsKey("#price"));
-            Assert.AreEqual(condition.ExpressionAttributeNames["#price"], "Price");
-            Assert.IsTrue(condition.ExpressionAttributeValues.ContainsKey(":price"));
-            Assert.AreEqual(condition.ExpressionAttributeValues[":price"], 50);
-        }
-
-        [TestMethod]
-        public void TestMethod4()
-        {
-            var condition = ComposedConditional.New()
-                    .WithCondition(ConditionalExpression.New().WithName("Price").EqualTo(50))
-                    .And(ConditionalExpression.New().WithName("Id").EqualTo(50))
-                    .Build();
-          
-
-            Assert.AreEqual("#price = :price AND #id = :id", condition.ExpressionStatement);
-            Assert.IsTrue(condition.ExpressionAttributeNames.ContainsKey("#price"));
-            Assert.AreEqual(condition.ExpressionAttributeNames["#price"], "Price");
-            Assert.IsTrue(condition.ExpressionAttributeValues.ContainsKey(":price"));
-            Assert.AreEqual(condition.ExpressionAttributeValues[":price"], 50);
-        }
-
-        [TestMethod]
-        public void TestMethod5()
-        {
-            var condition = ComposedConditional.New()
-                    .WithCondition(ConditionalExpression.New().WithName("Price").EqualTo(50))
-                    .And(FunctionExpression.New().AttributeExists("Test"))
-                    .Build();
-
-            Assert.AreEqual("#price = :price AND attribute_exists(#test)", condition.ExpressionStatement);
-            Assert.IsTrue(condition.ExpressionAttributeNames.ContainsKey("#price"));
-            Assert.AreEqual(condition.ExpressionAttributeNames["#price"], "Price");
-            Assert.IsTrue(condition.ExpressionAttributeValues.ContainsKey(":price"));
-            Assert.AreEqual(condition.ExpressionAttributeValues[":price"], 50);
-        }
-
-        [TestMethod]
-        public void TestMethod6()
-        {
-            var condition = UpdateExpression.New().WithName("Price").EqualTo(50)
-                .Build();
-
-
-            Assert.AreEqual("SET #price = :price", condition.ExpressionStatement);
-            Assert.IsTrue(condition.ExpressionAttributeNames.ContainsKey("#price"));
-            Assert.AreEqual(condition.ExpressionAttributeNames["#price"], "Price");
-            Assert.IsTrue(condition.ExpressionAttributeValues.ContainsKey(":price"));
-            Assert.AreEqual(condition.ExpressionAttributeValues[":price"], 50);
-        }
-
-
-        [TestMethod]
         public void TestMethod7()
         {
             var condition = ConditionBuilder.New()
